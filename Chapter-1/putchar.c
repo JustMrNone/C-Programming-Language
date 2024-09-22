994d818dae100272802ad78ffa335d01ec6b1ee9
Kamyar Bashar <kamyar.bashar@gmail.com>
2024-09-22 10:19:49 +0330

    up to file copying

---
+++ Chapter-1/putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    //reads next input character from the text stream 
+    char c = getchar();
+    //prints a character each time that it is called
+    putchar(c);
+    //using printf to do the samething 
+    printf("%c", c);
+}
